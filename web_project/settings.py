fda17a17cc47cd09f3f1659fb06c77ff1e462006
exam70486 <65937647+Exam70486@users.noreply.github.com>
2026-06-03 07:46:31 +0000

    working folder update

--- web_project/settings.py
+++ web_project/settings.py
@@ -88,7 +88,8 @@ DATABASES = {
         'PORT': '1433'
     },
     'OPTIONS': {
-        'driver': 'ODBC Driver 17 for SQL Server',
+        'driver': 'ODBC Driver 18 for SQL Server',
+        'TrustServerCertificate': 'yes',  # <-- ADD THIS LINE
     },
 }
 
